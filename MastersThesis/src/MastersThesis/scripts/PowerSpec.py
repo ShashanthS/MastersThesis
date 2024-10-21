5f788e7e8b47638be4c893a09ba715a9e428effa
Shashanth S <shashanth.s123@gmail.com>
2024-10-21 15:56:25 +0200

    Implemented Rebinning method

--- MastersThesis/src/MastersThesis/scripts/PowerSpec.py
+++ MastersThesis/src/MastersThesis/scripts/PowerSpec.py
@@ -9,6 +9,8 @@ from stingray import AveragedPowerspectrum, AveragedCrossspectrum, EventList
 from matplotlib import cm, ticker
 from astropy.io import fits
 
+import scipy.stats as scst
+
 from General import *
 
 def make_avg_periodogram(lc_counts, lc_times, norm=True, seg_size=64, dt=1/512):
@@ -45,11 +47,41 @@ def make_avg_periodogram(lc_counts, lc_times, norm=True, seg_size=64, dt=1/512):
     
     return avg_pow, freq_list[0]
 
-def avg_periodogram_wrapper(data_dir, seg_size, energy_range=[3, 10], plot=True):
+def rebin(x, y, f, n_stacked=0):
+    """
+    x must be linearly spaced
+    """
+    dx = x[1] - x[0]
+
+    # create new set of bins:
+    minx = 0
+    maxx = x[-1]
+    new_bins = [minx, minx+dx]
+    while new_bins[-1] <= maxx:
+        dx *= (1+f)
+        # print(new_bins)
+        new_bins.append(new_bins[-1] + dx)
+
+    new_bins = np.asanyarray(new_bins)
+    # print(new_bins)
+
+    rebinned_x, edges_x, indices_x = scst.binned_statistic(x, x, 'mean', new_bins)
+    # print(bins_x)
+    rebinned_y_real, edges_real, indices_real = scst.binned_statistic(x, y.real, 'mean', new_bins)
+
+    n_binned = [np.count_nonzero(indices_real[indices_real==i]) for i in range(len(rebinned_y_real))]
+    n_binned = np.asanyarray(n_binned)
+
+    # print(indices_real)
+    # print(rebinned_x)
+# 
+    return rebinned_x, rebinned_y_real, n_binned + n_stacked
+
+def avg_periodogram_wrapper(data_dir, seg_size, energy_range=[3, 10], plot=True, rebin_f=0):
     """
     energy_range must be of the form [E_min, E_max]
     """
-    eventlist = st.EventList.read(data_dir, "hea", additional_columns=['DET_ID']) # Load eventlist from file
+    eventlist = st.EventList.read(data_dir, "hea") # Load eventlist from file
     eventlist = eventlist.filter_energy_range(energy_range)
     
     print("Loaded Event List")
@@ -59,15 +91,24 @@ def avg_periodogram_wrapper(data_dir, seg_size, energy_range=[3, 10], plot=True)
     lc_gtis = lc_full.split_by_gti()
 
     split_counts, split_times, n_stacked = split_multiple_lc(lc_gtis, segment_size=seg_size)
-    avg_pow, xf = make_avg_periodogram(split_counts, split_times)
+    avg_pow, freq = make_avg_periodogram(split_counts, split_times)
+
+    avg_pow = avg_pow[freq>0]
+    freq = freq[freq>0]
+
+    rebinned_freq, rebinned_pow, total_stacked = rebin(freq, avg_pow, f=rebin_f, n_stacked=10)
 
     if plot:
         fig_p, ax_p = plt.subplots()
-
-        avg_pow = avg_pow[xf>0]
-        xf = xf[xf>0]
-
-        ax_p.plot(xf, avg_pow * xf, drawstyle="steps-mid", color="k", alpha=.5, ls='-.')
+        ax_p.plot(freq, avg_pow * freq, drawstyle="steps-mid", color="k", alpha=.5, ls='-.')
+        ax_p.plot(rebinned_freq, rebinned_pow * rebinned_freq)
 
         ax_p.set_yscale('log')
         ax_p.set_xscale('log')
+
+        ax_p.set_xlabel('Frequency (Hz)')
+        ax_p.set_ylabel('Power (Units)')
+        plt.show()
+    
+    return rebinned_pow, rebinned_freq, total_stacked
+
